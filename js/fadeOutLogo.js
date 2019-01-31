c3ecf18c9b173996d4f75b1eff9323f586148cb3
Fatur Arif Muchamad <faturarifm@gmail.com>
2019-01-31 08:23:02 +0700

    Update v0.0.0.3
    - Adding autoScrollArrow.js
    - Adding fadeOutLogo.js
    - Updating paintbucket.css
    - Updating index.html (auto scroll arrow, content on page about)

---
+++ js/fadeOutLogo.js
@@ -0,0 +1,12 @@
+
+/*source : https://codepen.io/nickcil/pen/sfutl*/
+
+$(window).scroll(function(){
+    $(".mainBanner").css("opacity", 1 - $(window).scrollTop() / 500);
+  });
+
+/*win.scroll(function(){
+  scrollPosition = win.scrollTop();
+  scrollRatio = 1 - scrollPosition / 300;
+  $(".top").css("opacity", scrollRatio);
+*/
