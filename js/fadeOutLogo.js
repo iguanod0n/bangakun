f7b9da9af1d1793a7fef7ebd295cb2affa73d716
Fatur Arif Muchamad <faturarifm@gmail.com>
2019-02-06 23:13:01 +0700

    Update V0.0.0.8
    - Adding font
    - neatly neated neater
    - adding assets
    - finishing footer

--- js/fadeOutLogo.js
+++ js/fadeOutLogo.js
@@ -5,6 +5,10 @@ $(window).scroll(function(){
     $(".mainBanner").css("opacity", 1 - $(window).scrollTop() / 500);
   });
 
+  $(window).scroll(function(){
+      $(".scrollArrow").css("opacity", 1 - $(window).scrollTop() / 500);
+    });
+
 /*win.scroll(function(){
   scrollPosition = win.scrollTop();
   scrollRatio = 1 - scrollPosition / 300;
